3f2b187b05bc411de4547e17b3968a13a93ee967
molchin <melposyrup@gmail.com>
2024-04-26 20:32:04 +0900

    section1 fin

--- Source/Window/Window.cpp
+++ Source/Window/Window.cpp
@@ -1,6 +1,7 @@
 #include "Window.h"
 
-Window::Window(WindowSize size, const std::string& title) : mSize(size), mTitle(title)
+Window::Window(WindowSize size, const std::string& title) 
+	: mSize(size), mTitle(title), mWindow(nullptr)
 {
 }
 
@@ -11,15 +12,26 @@ Window::~Window()
 
 void Window::Initialize()
 {
-	if (!glfwInit())
-		printf("Failed to initialize GLFW!");
+	if (!glfwInit()){
+		printf("Failed to initialize GLFW!\n");
+		abort();
+	}
 	else
-		printf("GLFW initialized successfully!");
+		printf("GLFW initialized successfully!\n");
 
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
 	mWindow = glfwCreateWindow(mSize.X, mSize.Y, mTitle.c_str(), nullptr, nullptr);
+
+	if (mWindow == nullptr)
+	{
+		printf("Failed to create window!\n");
+		abort();
+	}
+	else
+		printf("Window created successfully!\n");
+
 }
 
 void Window::Run()
